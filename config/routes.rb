14fed4f9c72a127e2b8541e1ed488fcaaf23b809
Davi Luis <daviluis323@gmail.com>
2019-12-11 00:11:39 -0300

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # Api definition
   namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      # We are going to list our resources here
-    end
+  namespace :v1 do
+  resources :users, only: [:show]
+  end
   end
 end
---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+  test 'should show user' do
+    get api_v1_user_url(@user), as: :json
+    assert_response :success
+    # Test to ensure response contains the correct email
+    json_response = JSON.parse(response.body)
+    assert_equal @user.email, json_response['email']
+  end
+end
