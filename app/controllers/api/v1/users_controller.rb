f2dfd80bcc47f290f8271203b63d9bf4a4a8c9b6
Davi Luis <davil@speediomail.com>
2019-12-11 11:30:33 -0300

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,38 +1,40 @@
-# frozen_string_literal: true
-
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  
-  # GET /users/1
+  before_action :check_owner, only: %i[update destroy]
+
   def show
-    render json: @user
+    options = { include: [:products] }
+    render json: UserSerializer.new(@user, options).serializable_hash
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: UserSerializer.new(@user).serializable_hash
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user, status: :ok
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-   # DELETE /users/1
   def destroy
     @user.destroy
     head 204
   end
 
   private
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
+  end
 
   # Only allow a trusted parameter "white list" through.
   def user_params
