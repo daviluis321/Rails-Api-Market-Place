459dfcdb421dcadc8ecfbb1b099911289d625a55
Davi Luis <daviluis323@gmail.com>
2019-12-10 23:21:11 -0300

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json } do
-  namespace :v1 do
-  # We are going to list our resources here
-  end
+    namespace :v1 do
+      # We are going to list our resources here
+    end
   end
 end
