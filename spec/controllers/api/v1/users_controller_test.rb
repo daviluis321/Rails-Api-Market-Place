e0810711e7267f3dea49693d229c5719d95dbb1c
Davi Luis <davil@speediomail.com>
2019-12-12 13:05:09 -0300

    Verify Status User

--- spec/controllers/api/v1/users_controller_test.rb
+++ spec/controllers/api/v1/users_controller_test.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 RSpec.describe  Api::V1::UsersController do
-  describe "Verify Api" do
-    it 'responds successfully' do
-      user = create(:user)
-      get :show, params: { id: user.id }
+  describe "Verify User Api" do
+
+    before do
+      @user = create(:user)
+    end
+
+    it 'responds a 200 response' do
+      get :show, params: { id: @user.id }
       expect(response).to have_http_status(200)
     end
+
+    it 'responds a 302 response (not authorized)' do
+      post :destroy, params: { id: @user.id }
+      expect(response).to have_http_status(403)
+    end
+
   end
 end
